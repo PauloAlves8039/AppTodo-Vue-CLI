175dd6351b1075535e36399f94f4119e35309563
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-17 09:30:43 -0300

    Inserindo todos em lista

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
     <dir class="container grid-xs py-2">
       <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logomarca da aplicação">
       <br>
-      <form>
+      <form @submit.prevent="addTodo(todo)">
         <div class="input-group">
-          <input type="text" class="form-input" placeholder="Novo todo">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo">
           <button class="btn btn-primary">Adicionar</button>
         </div>
-      </form>
+        {{ todos }}
+      </form>      
     </dir>
   </div>
 </template>
@@ -16,7 +17,16 @@
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return { todos: [], todo: { checked: false } }
+  },
+  methods: {
+    addTodo(todo){
+      todo.id = Date.now()
+      this.todos.push(todo)
+    }
+  },
 }
 </script>
 
