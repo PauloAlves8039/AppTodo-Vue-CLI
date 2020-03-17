bb04be59d1ff8b9df8afa9f4ea02e57d7a9de9c4
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-17 09:11:30 -0300

    Estrutura inicial da aplicação

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="" id="app">
+    <dir class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/logo.png" alt="Logomarca da aplicação">
+      <br>
+      <form>
+        <div class="input-group">
+          <input type="text" class="form-input" placeholder="Novo todo">
+          <button class="btn btn-primary">Adicionar</button>
+        </div>
+      </form>
+    </dir>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+  .img-logo{
+    max-width: 200px;
+    margin: 0 auto;
+  }
+</style>
