626154b35516e2e2057c2777c3bb8e73b8f81fc2
PauloAlves8039 <pj.alves547@gmail.com>
2020-03-18 19:46:31 -0300

    Usando Vuex para remoção de itens

--- src/store.js
+++ src/store.js
@@ -6,23 +6,44 @@ import VueX from 'vuex'
 
 Vue.use(VueX)
 
-const state = { todos: [] }
+const state = { todos: [], loading: false }
 
 const actions = {
     addTodo({ commit }, todo){
+        commit('setLoading', true)
         return new Promise(resolve => {
             setTimeout(() => {
                 todo.id = Date.now()
                 commit('addTodo', todo)
+                commit('setLoading', false)
                 resolve(todo)
             }, 1000)
         })
+    },
+    toggleTodo({ commit }, todo){
+        commit('toggleTodo', todo)
+    },
+    removeTodo({ commit }, todo){
+        commit('removeTodo', todo)
     }
 }
 
 const mutations = {
     addTodo(state, payload){
         state.todos.push(payload)
+    },
+    setLoading(state, payload){
+        state.loading = payload
+    },
+    toggleTodo(state, payload){
+        const index = state.todos.findIndex(item => item.id === payload.id)
+        if(index > -1){
+            const checked = !state.todos[index].checked
+            Vue.set(state.todos, index, {...state.todos[index], checked })      
+        }
+    },
+    removeTodo(state, payload){
+        state.todos = state.todos.filter(item => item.id !== payload.id)
     }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,32 +19,26 @@ export default {
   name: 'App',
   components: { Todo },
   data(){
-    return { todo: { checked: false }, loading: false }
+    return { todo: { checked: false }}
   },
   computed: {
     todos(){
       return this.$store.state.todos
+    },
+    loading(){
+      return this.$store.state.loading
     }
   },
   methods: {
     async addTodo(todo){
-      this.loading = true
       await this.$store.dispatch('addTodo', todo)
       this.todo = { checked: false }
-      this.loading = false      
     },
     toggleTodo(todo){
-      const index = this.todos.findIndex(item => item.id === todo.id)
-      if(index > -1){
-        const checked = !this.todos[index].checked
-        this.$set(this.todos, index, {...this.todos[index], checked })      
-      }
+      this.$store.dispatch('toggleTodo', todo)
     },
     removeTodo(todo){
-      const index = this.todos.findIndex(item => item.id === todo.id)
-      if(index > -1){
-        this.$delete(this.todos, index)
-      }
+      this.$store.dispatch('removeTodo', todo)
     }
   },
 }
